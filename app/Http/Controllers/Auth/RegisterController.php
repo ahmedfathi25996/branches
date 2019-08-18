34a5ffaa9af68a80e8361233ab12aa0c739a1258
elmancy2016 <medoelmancy@gmail.com>
2019-08-18 12:28:59 +0200

    testttetst

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,6 +63,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        //test mememem
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
